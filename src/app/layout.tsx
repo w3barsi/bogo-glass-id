1f24f1a6abb27293cfe1c5cc49c129e81aa66d36
w3barsi <darcybalaga@gmail.com>
2024-10-16 00:42:14 +0800

    moved viewport to own export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { type Viewport, type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -10,12 +10,16 @@ import { Toaster } from "~/components/ui/sonner";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "~/app/api/uploadthing/core";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Bogo Glass ID",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+};
+export const viewport: Viewport = {
+  initialScale: 1,
+  maximumScale: 1,
+  width: "device-width",
 };
 
 export default function RootLayout({
